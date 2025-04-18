e16ff2ffb3e1e93718be39367447bdf2af8a9e03
uahma <uahma001@doc.gold.ac.uk>
2025-04-18 07:48:14 +0100

    Creating database to store user data

---
+++ database.sql
@@ -0,0 +1,16 @@
+# Creating the database
+CREATE DATABASE IF NOT EXISTS smartWaste;
+USE smartWaste;
+
+# Creating the table for the users
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL, 
+    email VARCHAR(100) NOT NULL,
+    hashedPassword VARCHAR(255) NOT NULL,
+    score INT DEFAULT 0
+);
+
+# Creating the app user and granting priviliges
+CREATE USER IF NOT EXISTS 'smartWaste_app'@'localhost' IDENTIFIED BY 'smartWaste123'; 
+GRANT ALL PRIVILEGES ON smartWaste.* TO 'smartWaste_app'@'localhost';
