571c80fbe5b02dfcbd9be4d2ea887b0f95c5bc27
uahma <uahma001@doc.gold.ac.uk>
2025-04-18 08:03:29 +0100

    Importing modules and establishing database connection

---
+++ index.js
@@ -0,0 +1,52 @@
+// Importing the modules
+const express = require ('express');
+const ejs = require('ejs');
+const session = require ('express-session');
+const validator = require('express-validator');
+const expressSanitizer = require('express-sanitizer');
+const mysql = require('mysql2');
+
+// Creating the express application object
+const app = express();
+const port = 8000;
+
+// Telling Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Setting up the body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Setting up the express sanitizer
+app.use(expressSanitizer());
+
+// Setting up public folder (for css and statis js)
+app.use(express.static('public'));
+
+// Creating a session
+app.use(session({
+    secret: 'secretkey',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+
+// Setting up the database connection
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'smartWaste_app',
+    password: 'smartWaste123',
+    database: 'smartWaste'
+});
+
+// Connecting to the database
+db.connect((err) => {
+    if (err) {
+        console.log('Error connecting to database', err);
+    }
+    else {
+        console.log('Connected to the database');
+    }
+});
+global.db = db; // Making the database accessible for all routes
