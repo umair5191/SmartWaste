30aa94a534d313c2d6cbae24f1d93e6abc5e7d5e
uahma <uahma001@doc.gold.ac.uk>
2025-04-19 01:39:45 +0100

    Handling route to login

--- routes/users.js
+++ routes/users.js
@@ -4,3 +4,28 @@ const router = express.Router();
 const bcrypt = require('bcrypt'); // Importing bcrypt to securely store passwords by hashing them
 const saltRounds = 10;
 const { check, validationResult } = require('express-validator'); // Importing express-validator to check if inputs are valid
+
+// Handling route to login
+router.post('/loggedin', function (req, res, next) {
+    let sqlquery = "SELECT hashedPassword FROM users where username = ?";
+    db.query(sqlquery, [req.sanitize(req.body.username)], (err, result) => { // Sanitizing the username to prevent harmful attacks
+        if (err){
+            next(err);
+        }
+        if (result.length == 0) {
+            return res.render('index.ejs', { error: "Invalid username or password. Please try again." }); // If username is not found, user is informed and prompted to try again
+        }
+        let hashedPassword = result[0].hashedPassword;
+        // Comparing the password supplied with the password in the database
+        bcrypt.compare(req.sanitize(req.body.password), hashedPassword, function(err, result) { // Sanitizing the password to prevent harmful attacks
+            if (result == true) {
+                // Saving user session here when login is successful
+                req.session.userId = req.sanitize(req.body.username);
+                res.render('home.ejs', {username: req.sanitize(req.body.username)}); // If password is correct, user is welcomed to the home page
+            }
+            else {
+                res.render('index.ejs', { error: "Invalid username or password. Please try again." }); // If password is incorrect, user is informed and prompted to try again
+            }
+        })
+    })
+})
