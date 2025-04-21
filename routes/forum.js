4f4b6eae0d0414eae6a182c82c7d936020c5c395
uahma <uahma001@doc.gold.ac.uk>
2025-04-21 08:15:16 +0100

    Adding timeestamps to the posts

--- routes/forum.js
+++ routes/forum.js
@@ -2,10 +2,30 @@
 const express = require("express");
 const router = express.Router();
 
-// Handling the routes for the forum page
-router.get('/',function(req, res, next){
-    res.render('forum.ejs');
-})
+// Handling route to display forum page
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM posts ORDER BY date DESC', (err, results) => {
+    if (err) {
+      next(err);
+    }
+    res.render('forum.ejs', { posts: results });
+  });
+});
 
-// Exporting the router object so index.js can access it
+// Adding new post
+router.post('/post', (req, res) => {
+  const content = req.body.content;
+  const username = req.session.userId; 
+  const date = new Date();
+
+  // Inserting new post into the database
+  db.query('INSERT INTO posts (username, content, date) VALUES (?, ?, ?)', [username, content, date], (err, result) => {
+      if (err) {
+        next (err);
+      }
+      res.redirect('/forum'); // Refresh to show new post
+    }
+  );
+});
+
 module.exports = router;
