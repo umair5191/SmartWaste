7593fe9de57151ee58e4057617773d22b1a19ed2
uahma <uahma001@doc.gold.ac.uk>
2025-04-21 06:17:33 +0100

    Creating table for forum posts

--- database.sql
+++ database.sql
@@ -11,6 +11,14 @@ CREATE TABLE IF NOT EXISTS users (
     score INT DEFAULT 0
 );
 
+# Creating the table for forum posts
+CREATE TABLE IF NOT EXISTS posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    content TEXT NOT NULL,
+    date DATE NOT NULL
+);
+
 # Creating the app user and granting priviliges
 CREATE USER IF NOT EXISTS 'smartWaste_app'@'localhost' IDENTIFIED BY 'smartWaste123'; 
 GRANT ALL PRIVILEGES ON smartWaste.* TO 'smartWaste_app'@'localhost';
